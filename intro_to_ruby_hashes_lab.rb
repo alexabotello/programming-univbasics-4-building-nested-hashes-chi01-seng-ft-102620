7d3e3ab76371dd40d75c314e3392e5935ce1e619
Alexa Botello <abotello20@yahoo.com>
2020-10-19 20:27:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,4 @@
+require pry
 def base_hash
   monopoly = {
     :railroads=> {
@@ -6,7 +7,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-monopoly =  {
+monopoly = {
   :railroads=>
   {
     :pieces=>4,
@@ -21,7 +22,7 @@ monopoly =  {
 end
 
 def monopoly_with_third_tier
-monopoly =  {
+monopoly = {
   :railroads=>
   {
     :pieces=>4,
@@ -52,17 +53,10 @@ monopoly =  {
 end
 
 def monopoly_with_fourth_tier
-monopoly =  {
+monopoly = {
   :railroads=>
   {
     :pieces=>4,
-    :rent_in_dollars=>
-    {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    },
     :names=>
     {
       :reading_railroad=>
@@ -81,8 +75,14 @@ monopoly =  {
       {
         "mortgage_value"=>"$800"
       }
+    },
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
     }
   }
 }
-
-end
+end 
