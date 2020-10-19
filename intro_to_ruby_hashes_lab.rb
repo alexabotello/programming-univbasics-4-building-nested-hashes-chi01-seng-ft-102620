a8a27635d011ad1b522836073823e7f4a548136d
Alexa Botello <abotello20@yahoo.com>
2020-10-19 20:41:41 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,4 @@
+require "pry"
 def base_hash
   monopoly = {
     :railroads=> {
@@ -6,7 +7,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-monopoly =  {
+monopoly = {
   :railroads=>
   {
     :pieces=>4,
@@ -21,7 +22,7 @@ monopoly =  {
 end
 
 def monopoly_with_third_tier
-monopoly =  {
+monopoly = {
   :railroads=>
   {
     :pieces=>4,
@@ -52,37 +53,38 @@ monopoly =  {
 end
 
 def monopoly_with_fourth_tier
-monopoly =  {
+monopoly = {
   :railroads=>
   {
     :pieces=>4,
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        :mortgage_value=>100
+      },
+      :pennsylvania_railroad=>
+      {
+        :mortgage_value=>200
+      },
+      :b_and_o_railroad=>
+      {
+        :mortgage_value=>400
+      },
+      :shortline_railroad=>
+      {
+        :mortgage_value=>800
+      }
+    },
     :rent_in_dollars=>
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
-    },
-    :names=>
-    {
-      :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      },
-      :pennsylvania_railroad=>
-      {
-        "mortgage_value"=>"$200"
-      },
-      :b_and_o_railroad=>
-      {
-        "mortgage_value"=>"$400"
-      },
-      :shortline=>
-      {
-        "mortgage_value"=>"$800"
-      }
     }
   }
 }
-
-end
+# binding.pry
+end 
+# expect(monopoly_with_fourth_tier[:railroads][:names][:reading_railroad][:mortgage_value]).to eq(100)
